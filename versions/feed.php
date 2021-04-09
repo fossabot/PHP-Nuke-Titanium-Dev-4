4a2998cf55f716d439698341b2178a7684027cde
Ernest Buffington <ernest.buffington@gmail.com>
2021-04-08 21:48:08 -0400

    Versions
    Versions Update

---
+++ versions/feed.php
@@ -0,0 +1,69 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Live Feed from PHP-Nuke Titanium Hub</title>
+</head>
+<body> 
+<strong>SEPTEMBER 2019 - Ernest Buffington</strong> 
+<hr>
+<strong>Updated jquery-3.3.1.min.js to jquery-3.4.1.js - Sep 18, 2019 </strong><br />
+A few Bug fixes were taken care of in this version.<br /> 
+<strong>Updated Loan Module - for PHP 7.3.8 - Sep 18, 2019 </strong><br />
+Added some more space at the bottomof each table.<br /> 
+<strong>Titanium Network Projects v11.11 - for PHP 7.3.8 - Sep 16, 2019 </strong><br />
+Ported over from Titanium 1.0.1z<br /> 
+<strong>Portal Menu Fixes - for PHP 7.3.8 - Sep 15, 2019 </strong><br />
+Fixed Dynamic Links Drop Down for External Links<br /> 
+<strong>Arcade Fixes - for PHP 7.3.8 - Sep 15, 2019 </strong><br />
+comments.php, comments_list.php, whoisplaying.php and comments_new.php<br /> 
+<strong>Added CKEditor 4 - for PHP 7.3.8 - Sep 8, 2019 </strong><br />
+Created a custom version of CKEditor 4 for PHP-Nuke Titanium v3.0.0<br /> 
+<strong>Block block-Portal_Menu.php updated to v5.01 - for PHP 7.3.8 - Sep 8, 2019 </strong><br /> 
+created auto installer and fixed the keys and indexes on the database!<br />
+<strong>Fixed Blog_Archive Module - Sep 8, 2019 </strong><br />
+Show All Stories Icons were missing!<br />
+<strong>Fixed Blog Module - Sep 8, 2019 </strong><br />
+Categories Icons Print, Friend and Edit were missing!<br />
+<strong>Fixed Evo_UserBlock- Sep 7, 2019 </strong><br />
+Added groups pending and updated group memberships! Both are clickable links Now!<br />
+<hr>
+<strong>New hub.86it.us GitHub Repsoitory setup - Sept 1, 2019 </strong><br />
+<a href="https://github.com/ernestbuffington/The-86it-Developers-Network" target="_blank">https://github.com/ernestbuffington/The-86it-Developers-Network</a>
+<hr>
+<br />
+<strong>AUGUST 2019 - Ernest Buffington</strong><br /> 
+<hr>
+<strong>Block block-Portal_Menu.php updated to v5.01 - for PHP 7.3.6 - Aug 28, 2019 </strong><br /> 
+<strong>Added back Blogs Configuration Admin Function - for PHP 7.3.6 - Aug 27, 2019 </strong><br /> 
+<strong>Added back Blogs Submission Admin Function - for PHP 7.3.6 - Aug 27, 2019 </strong><br />
+<strong>Created Seperate Blogs Last 100 Admin Function - for PHP 7.3.6 - Aug 27, 2019 </strong><br />
+<strong>Created Seperate Blogs Programmed Admin Function - for PHP 7.3.6 - Aug 27, 2019 </strong><br />
+<strong>We had to 86 the Shout_Box Module - Broken in PHP 7.3.6 - Aug 26, 2019 </strong><br />
+<strong>We had to 86 the Submit_News Module - Aug 26, 2019 </strong><br />
+<strong>We had to 86 the Top Module - Aug 26, 2019 </strong><br />
+<strong>We had to 86 the Sories_Archive Module - Aug 26, 2019 </strong><br />
+<strong>We had to 86 the Topics Module - Aug 26, 2019 </strong><br />
+<strong>Created Blog_Submit Module - Aug 26, 2019 </strong><br />
+<strong>Created Blog_Top Module (now only supports top 10 Blogs)- Aug 26, 2019 </strong><br />
+<strong>Created Blog_Archive Module - Aug 26, 2019 </strong><br />
+<strong>Created Blog_Topics Module - Aug 26, 2019 </strong><br />
+<strong>Updated Aracade Templates for Titanium Theme - Aug 25, 2019 </strong><br />
+<strong>Updated Aracade Templates for CarbinFiber Blue Skulls Theme - Aug 25, 2019 </strong><br />
+<strong>Updated Aracade Templates for CarbinFiber Red Flames Theme - Aug 25, 2019 </strong><br />
+<strong>Changed Admin Live Feed : Using Curl like it should be - Aug 12, 2019 </strong><br />
+<strong>Web Links Module : added new reCAPTCHA - Aug 11, 2019 </strong><br />
+<strong>TegoNuke(tm) Download Module : added new reCAPTCHA - Aug 10, 2019 </strong><br />
+<strong>TegoNuke(tm) Download Module : made functional in PHP 7.3.6 - Aug 9, 2019 </strong><br />
+<strong>Finished CarbinFiber Red Flames Theme - Aug 9, 2019 </strong><br />
+<strong>Finished CarbinFiber Blue Skulls Theme - Aug 9, 2019 </strong><br />
+<strong>Ported Over Titanium Theme - Aug 9, 2019 </strong><br />
+<strong>Portal Menu v5.01 is now functional in PHP 7.3.6 - Aug 8, 2019 </strong><br /><br />
+
+<strong>SEPTEMBER 2017 - Ernest Buffington</strong>
+<hr>
+<strong>New PHP-Nuke Titanium GitHub Repsoitory setup - Sept 4, 2017 </strong><br />
+<a href="https://github.com/ernestbuffington/PHP-Nuke-Titanium" target="_blank">https://github.com/ernestbuffington/PHP-Nuke-Titanium</a>
+<hr>
+</body>
+</html>
