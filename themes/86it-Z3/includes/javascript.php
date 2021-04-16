e9fc4065a18e21042ae13d0f310edc260441a7cb
Ernest Buffington <ernest.buffington@gmail.com>
2021-04-16 16:13:01 -0400

    Update
    Theme Updates

--- themes/86it-Z3/includes/javascript.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (realpath(__FILE__) == realpath($_SERVER['SCRIPT_FILENAME'])) 
-{
-    exit('Access Denied');
-}
-global $ThemeSel;
-echo "<script language=\"javascript\" src=\"themes/".$ThemeSel."/js/liveclock.js\"></script>\n";
-?>
